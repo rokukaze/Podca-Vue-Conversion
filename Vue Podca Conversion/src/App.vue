fef3bcb0cf44a20b4ac4dbf9bd7dbadb54a36ce3
Naz <naseemhammoud@gmail.com>
2020-09-05 18:47:49 -0600

    Converted podca footer to component

--- Vue Podca Conversion/src/App.vue
+++ Vue Podca Conversion/src/App.vue
@@ -2,27 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PodcaFooter/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PodcaFooter from './components/PodcaFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, PodcaFooter
   }
 }
 </script>
 
 <style>
-#app {
+/* Removed for proper podca formatting */
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
