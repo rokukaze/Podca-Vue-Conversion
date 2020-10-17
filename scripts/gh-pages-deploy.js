ff22bcd161b0addfc026b980adffa9c179e032e3
Naz <easybreezesolutions@gmail.com>
2020-10-17 11:11:37 -0700

    Github actions update

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -15,7 +15,7 @@ const fs = require("fs");
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
         await execa("rm", ["-r", folderName]);
-        await execa("git", ["checkout", "-f", "Development"]);
+        await execa("git", ["checkout", "-f", "development"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed, check your settings");
     } catch (e) {
