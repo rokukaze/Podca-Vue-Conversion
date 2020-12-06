e7a84b06a9d8c7e1149ce57bce029bc08e303a08
Naz <17075595+rokukaze@users.noreply.github.com>
2020-12-06 02:10:30 -0800

    Added default user img and behind the mic subsection component

--- src/components/BehindTheMic.vue
+++ src/components/BehindTheMic.vue
@@ -107,25 +107,12 @@
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
-            <div class="team-member">
-
-              <img src="images/person_6.jpg" alt="Image" class="img-fluid">
-
-              <div class="text">
-
-                <h2 class="mb-2 font-weight-light h4">Brooke Cagle</h2>
-                <span class="d-block mb-2 text-white-opacity-05">Creative Director</span>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit ullam reprehenderit nemo.</p>
-                <p>
-                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
-                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
-                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
-                </p>
-              </div>
-
-            </div>
-          </div>
+          <BehindTheMicTeamMember
+            teamMemberName="Brooke Cagle"
+            teamMemberTitle="Creative Director"
+            teamMemberDescription="More lorem ipsum"
+            imageSource="images/person_6.jpg"
+          />
 
 
         </div>
@@ -134,7 +121,9 @@
 </template>
 
 <script>
+import BehindTheMicTeamMember from './BehindTheMicTeamMember.vue'
 export default {
+  components: { BehindTheMicTeamMember },
   name: 'BehindTheMic'
 }
 </script>
---
+++ src/components/BehindTheMicTeamMember.vue
@@ -0,0 +1,50 @@
+<template>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5">
+            <div class="team-member">
+
+              <img :src="imageSource" alt="Image" class="img-fluid">
+
+              <div class="text">
+
+                <h2 class="mb-2 font-weight-light h4">{{teamMemberName}}</h2>
+                <span class="d-block mb-2 text-white-opacity-05">{{teamMemberTitle}}</span>
+                <p class="mb-4">{{teamMemberDescription}}</p>
+                <p>
+                  <a href="#" class="text-white p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="text-white p-2"><span class="icon-linkedin"></span></a>
+                </p>
+              </div>
+
+            </div>
+          </div> 
+</template>
+
+<script>
+export default {
+  name: 'BehindTheMicTeamMember',
+  props:{
+      imageSource: {
+          type: String,
+          default: "images/defaultavatar.png"
+      },
+      teamMemberName: {
+          type: String,
+          default: "John Doe"
+      },
+      teamMemberTitle: {
+          type: String,
+          default: "Podcast Member"
+      },
+      teamMemberDescription: 
+      {
+          type: String,
+          default: "Lorem Ipsum"
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
